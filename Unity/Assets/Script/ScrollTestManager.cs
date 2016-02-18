d59761980b7558107d5970ce06a499c85b738e75
Yasuo Matsui <gm25yasu@gmail.com>
2016-02-18 17:08:22 +0900

    uguiのスクロールサンプル追加

---
+++ Unity/Assets/Script/ScrollTestManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollTestManager : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject _scroll_content;
+	[SerializeField]
+	private GameObject _list;
+
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 30; i++) {
+			GameObject obj = Instantiate (_list) as GameObject;
+			obj.transform.SetParent (_scroll_content.transform, false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
